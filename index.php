8bfc9e19e149b0ee388476622bafd6dc705c457d
timothytyang <timothy.t.yang@gmail.com>
2016-05-22 00:07:22 -0500

    testing

--- index.php
+++ index.php
@@ -51,7 +51,7 @@
 
     <div class="container">
 
-      <form class="form-signin" method="post">
+      <form class="form-signin" method="GET">
         <h2 class="form-signin-heading">Please sign in</h2>
         <label for="inputEmail" class="sr-only">Email address</label>
         <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
