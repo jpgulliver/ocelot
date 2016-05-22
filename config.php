d7bd1b7f08688603ea05b557cf07de94f1134cda
Jon Gulliver <jpgulliver@wisc.edu>
2016-05-22 12:55:54 -0500

    separate arg for port # in config.php

--- config.php
+++ config.php
@@ -1,9 +1,10 @@
 <?php
-   define('DB_SERVER', 'ocelot.cfxx0yyzf2du.us-west-2.rds.amazonaws.com:3306');
+   define('DB_SERVER', 'ocelot.cfxx0yyzf2du.us-west-2.rds.amazonaws.com');
    define('DB_USERNAME', 'admin');
    define('DB_PASSWORD', 'coatolivetap1!');
    define('DB_DATABASE', 'ocelotDB');
-   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+   define('DB_PORT', '3306');
+   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE, DB_PORT);
    if (!$db) {
       die("Connection failed: " . mysqli_connect_error());
    }
